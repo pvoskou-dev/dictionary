cb7a4b1b7c3ad9ee7c9b21443058522a3edede0f
pvoskou <pvoskou.developer@gmail.com>
2023-02-15 09:47:08 +0100

    added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'dictionary-493a9',
+    appId: '1:313175482764:web:cea4552c4f43e4a718b6a9',
+    storageBucket: 'dictionary-493a9.appspot.com',
+    apiKey: 'AIzaSyDzTyg-SM5DIE104IK-3vVRncQj-3703ew',
+    authDomain: 'dictionary-493a9.firebaseapp.com',
+    messagingSenderId: '313175482764',
+    measurementId: 'G-6L6XKXZ8VD',
+  },
   production: true
 };
